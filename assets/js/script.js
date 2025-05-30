5942cd1ae9b76850c4ac870f960abe37f5bec19d
LuuizPaes <lu.fernando.paes@hotmail.com>
2025-05-30 09:26:50 -0300

    Inicando a parte do js

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+const mode = document.querySelector("#mode_icon");
+
+mode.addEventListener("click", () => {
+  if (mode.classList.contains("fa-moon")) {
+    mode.classList.remove("fa-moon");
+    mode.classList.add("fa-sun");
+
+    return;
+  }
+
+  mode.classList.add("fa-moon");
+  mode.classList.remove("fa-sun");
+});
